f840d4d4891d0045c3ca6884458e857a9c4e96a2
sahar <sahar.nnad@gmail.com>
2023-04-15 16:44:49 +0100

    Header Done

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+const Header = ()=>{
+    return (
+      <header className="main-header">
+        <div class="container">
+          <a href="/">Digital Agency</a>
+
+          <nav class="main-nav">
+            <ul class="main-nav-list">
+              <li>Home</li>
+              <li>about</li>
+              <li>testimonials</li>
+              <li>Contant</li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    );
+}
+
+
+ 
+export default Header
---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./../scss/components/Header.scss";
+
+const heroSection = () => {
+  return (
+    <section>
+      <div className="contain-part">
+        <div className="text-part"></div>
+        <div className="photo-part"></div>
+      </div>
+    </section>
+  );
+};
+
+export default heroSection;
